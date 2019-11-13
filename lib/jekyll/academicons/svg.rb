af0fda8dc168b085ecaf7bc9463cefd0c8705665
Sylvain METAYER <contact@sylvainmetayer.fr>
2019-11-13 20:57:34 +0100

    initial commit

---
+++ lib/jekyll/academicons/svg.rb
@@ -0,0 +1,10 @@
+require "jekyll/academicons/svg/version"
+
+module Jekyll
+  module Academicons
+    module Svg
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/academicons/svg/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Academicons
+    module Svg
+      VERSION = "0.1.0"
+    end
+  end
+end
